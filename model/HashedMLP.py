e3fdc48438b7d91737827229df9e4d8831fea693
kazewong <kazewong.physics@gmail.com>
2022-01-28 22:31:45 -0500

    Finish implementing hashed interpolator

--- model/HashedMLP.py
+++ model/HashedMLP.py
@@ -5,20 +5,32 @@ import itertools
 
 large_prime = [1, 19349663, 83492791, 48397621]
 
-class HashInterpolator(nn.Module):
-    def __init__(self, n_dim: int, n_entries: int, n_feature: int):
+class HashedInterpolator(nn.Module):
+    def __init__(self, n_dim: int, n_entries: int, n_feature: int, grids: torch.tensor):
         super().__init__()
         self.n_dim = n_dim
         self.n_entries = n_entries
         self.n_feature = n_feature
+        self.grids = grids
         self.hash_table = nn.Parameter((torch.rand(n_entries,n_feature)-0.5)*2e-4)
         self.bit_table = torch.tensor(list(itertools.product([0,1],repeat=n_dim)))
         self.index_list = torch.repeat_interleave(torch.arange(n_dim)[None,:],2**n_dim,dim=0)
 
-    def findGrid(self,*position):
-        pass
+    def findGrid(self,position):
+        """
+        Find the corresponding lower corner of the input position.
+        Assume the position are in the range of [0,1]^n_dim.
+    
+        Args:
+            position: A tensor of shape (batch_size, n_dim)
+        Returns:
 
-    def getData(self,lower_corner,data):
+        """
+        corner_index = torch.floor(position*self.grids).int()
+        return corner_index
+        
+
+    def getData(self,lower_corner):
         """
         Get data using lower corner indices and data array.
 
@@ -29,7 +41,8 @@ class HashInterpolator(nn.Module):
             A tensor of shape (batch_size, n_dim)
         """
         corner_index = torch.repeat_interleave(lower_corner[:,None],2**self.n_dim,dim=1)+self.bit_table
-        corner_value = data[corner_index.chunk(self.n_dim,dim=-1)][...,0]
+        corner_hash = self.hashing(corner_index)
+        corner_value = self.hash_table[corner_hash]
         return corner_value
 
 
@@ -44,7 +57,6 @@ class HashInterpolator(nn.Module):
             A tensor of shape (batch_size)
         """
         coef_list = torch.stack([position,1-position],axis=-1)
-        print(corner_value.shape,coef_list.shape)
         result = corner_value*torch.prod(coef_list[:,self.index_list,self.bit_table],dim=2)
         return torch.sum(result,axis=1)
 
@@ -58,10 +70,13 @@ class HashInterpolator(nn.Module):
             A tensor of shape (batch_size)
 
         """
-        hash = torch.zeros(index.shape[0],dtype=torch.int32)
+        hash = torch.zeros(index.shape[:-1],dtype=torch.int32)
         for i in range(self.n_dim):
-            hash = torch.bitwise_xor(hash,index[:,i]*large_prime[i]) % self.n_entries
+            hash = torch.bitwise_xor(hash,index[...,i]*large_prime[i]) % self.n_entries
         return hash
 
-    def forward(self, index: torch.tensor) -> torch.tensor:
-        pass
+    def forward(self, position: torch.tensor) -> torch.tensor:
+        lower_corner = self.findGrid(position)
+        corner_value = self.getData(lower_corner)
+        result = torch.stack([self.ndLinearInterpolation(corner_value[...,i],position) for i in range(self.n_feature)],axis=-1)
+        return result
