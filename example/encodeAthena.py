15b06214ae3d7a61ebd99265adc3ed90c9adabf0
Kaze Wong <kazewong.physics@gmail.com>
2022-02-01 12:00:36 -0500

    Update gitignore. Add grid data dataset Add validation to the Athena example.

--- example/encodeAthena.py
+++ example/encodeAthena.py
@@ -1,5 +1,6 @@
 from matplotlib import scale
 from model.HashedMLP import HashedMLP
+from dataset.GridDataset import GridDataset
 import torch
 import torch.nn as nn
 import numpy as np
@@ -7,15 +8,16 @@ import h5py
 import copy
 from sklearn.preprocessing import StandardScaler,QuantileTransformer
 from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader,random_split
 
 n_input = 3
-n_output = 1
+n_output = 5
 n_hidden = 64
 n_layers = 2
 n_entries = 2**19
 n_feature = 2
 base_grids = torch.tensor([16,16,16]).cuda()
-n_level = 12
+n_level = 16
 n_factor = 1.5
 n_auxin = 0
 act = nn.ReLU()
@@ -30,53 +32,61 @@ learning_rate = 0.0001
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(0.9,0.99), eps=1e-8,weight_decay=1e-4)
 batch_size = 1000000
 n_epoch = 30000
-n_check = 50
+verbose_epoch = 10
 loss = nn.MSELoss()
 writer = SummaryWriter("./runs/mixing_layer")
 
-stride = 2
-data = h5py.File("/mnt/ceph/users/wwong/Simulations/AstroSimChallenge/Athena/mixing_layer.hdf5",'r')['rho'][::stride,::stride,::stride][...,None]
-#data = h5py.File("/mnt/ceph/users/wwong/Simulations/AstroSimChallenge/Athena/plasmoid_static.hdf5",'r')['rho'][::stride,::stride,::stride][...,None]
-data = torch.tensor(data).float()
+stride = 4
+dataset = GridDataset("/mnt/ceph/users/wwong/Simulations/AstroSimChallenge/Athena/mixing_layer.hdf5",["rho","press","vel1","vel2","vel3"],stride=stride)
+train_data, val_data = random_split(dataset, [int(0.8*len(dataset)), len(dataset)-int(0.8*len(dataset))])
 
-original_shape = data.shape
-coord = torch.stack(torch.meshgrid(torch.arange(data.shape[0]),torch.arange(data.shape[1]),torch.arange(data.shape[2])),-1).float()
-coord[...,0] = coord[...,0]/data.shape[0]
-coord[...,1] = coord[...,1]/data.shape[1]
-coord[...,2] = coord[...,2]/data.shape[2]
-coord = torch.flatten(coord,end_dim=-2)
-data = torch.flatten(data,end_dim=-2)
+best_train_loss = 1e10
+best_val_loss = 1e10
 
-# data = np.log10(data)
-#scaler = StandardScaler()
-# scaler = QuantileTransformer()
-# scaler.fit(data.detach().cpu().numpy())
-# data = torch.tensor(scaler.transform(data.detach().cpu().numpy())).float()
 
-writer.add_gradph(model,coord[1:2])
-best_loss = 1e10
-
-for epoch in range(n_epoch):
+def train(epoch):
+    model.train()
     running_loss = 0.0
-    for i in range(0,data.shape[0],batch_size):
+    for i in range(0,train_data.__len__(),batch_size):
         optimizer.zero_grad()
-        output = model(coord[i:i+batch_size])
-        loss_value = loss(output,data[i:i+batch_size].to(device))
+        coord, data = train_data[i:i+batch_size]
+        output = model(coord)
+        loss_value = loss(output,data.to(device))
         loss_value.backward()
         optimizer.step()
         running_loss += loss_value.item()
-        if i == int(data.shape[0]/batch_size)*batch_size:
-            print('loss: %.3f' % (running_loss*batch_size/data.shape[0]))
-        if running_loss < best_loss:
-            best_loss = running_loss
-            writer.add_scalar('training_loss',running_loss*batch_size/data.shape[0],epoch*data.shape[0]+i)
-            #torch.save(model.module.state_dict(), './checkpoint/Athena_turbulence_512_best.pth')
-            torch.save(model.module.state_dict(), './checkpoint/Athena_mixing_layer_256_best.pth')
+        if i == int(train_data.__len__()/batch_size)*batch_size:
+            if epoch % verbose_epoch == 0:
+                print('training loss: %.3f' % (running_loss*batch_size/data.shape[0]))
+            torch.save(model.module.state_dict(), './checkpoint/Athena_mixing_layer_all_best_train.pth')
+            writer.add_scalar('loss/train', running_loss*batch_size/data.shape[0], i)
+            running_loss = 0.0
 
-
-pred = []
-for i in range(0,data.shape[0],batch_size):
+def validate(epoch):
+    model.eval()
+    running_loss = 0.0
     with torch.no_grad():
-        output = model(coord[i:i+batch_size].to(device))
-        pred.append(output.cpu().numpy())
-pred = np.concatenate(pred,axis=0).reshape(original_shape)
+        for i in range(0,val_data.__len__(),batch_size):
+            coord, data = val_data[i:i+batch_size]
+            output = model(coord)
+            loss_value = loss(output,data.to(device))
+            running_loss += loss_value.item()
+            if i == int(val_data.__len__()/batch_size)*batch_size:
+                if epoch % verbose_epoch == 0:
+                    print('validation loss: %.3f' % (running_loss*batch_size/data.shape[0]))
+                torch.save(model.module.state_dict(), './checkpoint/Athena_mixing_layer_all_best_val.pth')
+                writer.add_scalar('loss/val', running_loss*batch_size/data.shape[0], i)
+                running_loss = 0.0
+
+print("Start training")
+for epoch in range(n_epoch):
+    train(epoch)
+    if epoch%2 == 0:
+        validate(epoch)
+
+# pred = []
+# for i in range(0,data.shape[0],batch_size):
+#     with torch.no_grad():
+#         output = model(coord[i:i+batch_size].to(device))
+#         pred.append(output.cpu().numpy())
+# pred = np.concatenate(pred,axis=0).reshape(original_shape)
