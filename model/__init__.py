fd99ace8a6a87470f0e6906a32fc62268742551c
Wenda Zhou <wz2247@nyu.edu>
2022-02-04 15:03:11 -0500

    upload modified version of hashed mlp

---
+++ model/__init__.py
@@ -0,0 +1,6 @@
+"""Implementation of compression using gigapixel image ideas from:
+"Instant Neural Graphics Primitives with a Multiresolution Hash Encoding", Muller et al.
+
+https://github.com/NVlabs/instant-ngp
+
+"""
