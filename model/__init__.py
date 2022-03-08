974628c4cb84377a582dcdcc2d5e4805a84de749
Wenda Zhou <wz2247@nyu.edu>
2022-03-08 14:44:38 -0500

    delete old version

--- model/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Implementation of compression using gigapixel image ideas from:
-"Instant Neural Graphics Primitives with a Multiresolution Hash Encoding", Muller et al.
-
-https://github.com/NVlabs/instant-ngp
-
-"""
